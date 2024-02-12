7399b5ba2ebb8c5dc1f6c5abfde9d75d5c07debc
Phạm Việt <brogun2002@gmail.com>
2024-02-12 16:21:51 +0700

    add functions

--- control_flow.cpp
+++ control_flow.cpp
@@ -4,6 +4,8 @@
 #include <math.h>
 using namespace std;
 
+
+// Check whether odd or even number
 int even_odd_number(int num)
 {
     if(num % 2 == 0)
@@ -17,6 +19,7 @@ int even_odd_number(int num)
     return num;
 }
 
+// input number
 void input_even_odd()
 {
     int number ;
@@ -37,6 +40,7 @@ void input_even_odd()
     }
 }
 
+// Swap numbers function
 void swap_two_numbers(int *a, int *b)
 {
     int temp;
@@ -45,6 +49,7 @@ void swap_two_numbers(int *a, int *b)
     *b = temp;
 }
 
+// Sort numbers function
 void sort_number(int* arr, int n)
 {
     for (int i = 0; i < n; i++)
@@ -60,12 +65,54 @@ void sort_number(int* arr, int n)
     }
 }
 
+// Find max element in an array
+int max_number(int *arr, int n)
+{
+    int max = arr[0]; 
+    for (int i = 0; i < n; i++)
+    {
+        if(max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+// Find sum in an array
+int sum_of_array(int *arr, int n)
+{
+    int sum = 0 ;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+// Reverse element in an array
+void reverse_array(int* arr, int n)
+{
+    int arr2[100];
+    int i;
+    for (i = 0; i < n/2; i++)
+    {
+        swap_two_numbers(&arr[i], &arr[n - i - 1]);
+    }
+}
+
+
+// Input function
 void input_array()
 {
+    // Nhập n 
     int n;
     cout << "Nhập n: ";
     cin >> n;
 
+    // Nhập n phần tử
     int arr[100];
     cout << "Nhập " << n <<  " số sau: ";
     for (int i = 0; i < n; i++)
@@ -73,8 +120,30 @@ void input_array()
         cin >> arr[i] ;
     }
 
+    // Sort number
     sort_number(arr, n);
     cout << "Sort number: " ;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " " ;
+    }
+    
+    cout << endl;
+
+    // Find max element
+    int max = max_number(arr, n);
+    cout << "Largest number is: " << max;
+
+    cout << endl;
+    
+    // Find sum of array
+    int sum = sum_of_array(arr, n);
+    cout << "Sum is: " << sum;
+
+
+    // Reverse element in an array
+    reverse_array(arr, n);
+    cout << "Reverse number: " ;
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " " ;
